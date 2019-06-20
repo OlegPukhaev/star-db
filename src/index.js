fe6fc2686bb30bf2e5985085b900184c04c23520
Oleg Pukhaev <nuba8184@gmail.com>
2019-06-20 09:29:56 +0600

    Create empty application and test fetch request

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const getResourse = async (url) => {
+  const res =  await fetch(url);
+  const body = await res.json();
+  return body;
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+getResourse('https://swapi.co/api/people/1/')
+  .then( body => {
+    console.log("Get data:", body);
+  })
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
