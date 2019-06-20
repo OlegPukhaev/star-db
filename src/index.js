1aa2acbfc13da43dd4ecd0f727611a666045e507
Oleg Pukhaev <nuba8184@gmail.com>
2019-06-20 10:25:21 +0600

    API-client for starDB

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-const getResourse = async (url) => {
-  const res =  await fetch(url);
-  const body = await res.json();
-  return body;
+class SwapiService {
+
+  _apiBase = 'https://swapi.co/api';
+
+  async getResourse(url) {
+    const res =  await fetch(`${this._apiBase}${url}`); 
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}  +  recieved ${res.status}`);
+    }
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResourse(`/people/`);
+    return res.results;
+  }
+  getPerson(id) {
+    return this.getResourse(`/people/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResourse(`/starships/`);
+    return res.results;
+  }
+  getStarship(id) {
+    return this.getResourse(`/starships/${id}`);
+  }
+  async getAllPlanets() {
+    const res = await this.getResourse(`/planets/`);
+    return res.results;
+  }
+  getPlanet(id) {
+    return this.getResourse(`/planets/${id}`);
+  }
+
 }
 
-getResourse('https://swapi.co/api/people/1/')
-  .then( body => {
-    console.log("Get data:", body);
-  })
+const swapi = new SwapiService();
 
+swapi.getAllPeople().then( people => {
+  people.forEach(element => {
+    console.log(element.name);
+  });
+});
 
+swapi.getPerson(3).then( person => {
+  console.log("Person Name: ", person.name);
+});
